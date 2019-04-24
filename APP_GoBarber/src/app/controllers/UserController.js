44576546b5cc783b941cba67f09455a1fe3692a6
André Luís <andreljs2013@gmail.com>
2019-04-23 21:47:24 -0300

    Atualização

--- APP_GoBarber/src/app/controllers/UserController.js
+++ APP_GoBarber/src/app/controllers/UserController.js
@@ -6,9 +6,9 @@ class UserController {
   }
 
   async store(req, res) {
-    req.body.avatar = "teste.jpg";
-      
-    await User.create(req.body);
+    const { filename } = req.file;
+
+    await User.create({ ...req.body, avatar: filename });
 
     return res.redirect("/");
   }
