0c866d2656f66421f291013d074abbba1c1042ac
Andre <projetocvjobs@gmail.com>
2019-04-27 12:58:29 -0300

    API REST Node.js

---
+++ GoNode/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("Login");
+});
+
+app.listen(3000);
